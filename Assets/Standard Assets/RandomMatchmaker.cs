f412d10da8663dc36564adee79656668e01c7b6a
Blake <blakelmyers@gmail.com>
2014-03-29 14:04:11 -0500

    Take out waiting for better testing

--- Assets/Standard Assets/RandomMatchmaker.cs
+++ Assets/Standard Assets/RandomMatchmaker.cs
@@ -65,7 +65,11 @@ public class RandomMatchmaker : Photon.MonoBehaviour
 			
 		if(playerID == 1)  // Player 1
 		{
-			waiting = true;
+			//waiting = true;
+
+			monster = PhotonNetwork.Instantiate(playerChoice, startingPosPlayer1, Quaternion.identity, 0);
+			monster.tag = "Red";
+			myPhotonView = monster.GetComponent<PhotonView>();
 		}
 			 
 		if(playerID == 2)
@@ -83,11 +87,12 @@ public class RandomMatchmaker : Photon.MonoBehaviour
 		if(player.ID == 2)
 		{
 			if(playerID == 1)  // Player 1 
-			{
+			{   /*
 				GameObject monster;
 				monster = PhotonNetwork.Instantiate(playerChoice, startingPosPlayer1, Quaternion.identity, 0);
 				monster.tag = "Red";
 				myPhotonView = monster.GetComponent<PhotonView>();
+				*/
 			}
 		}
 	}
